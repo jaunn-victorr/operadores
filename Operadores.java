8e79ba945df00e1cda4794eed7f26b3495c7ed56
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-03-09 17:46:52 +0000

    save05

--- Operadores.java
+++ Operadores.java
@@ -6,6 +6,7 @@ public class Operadores {
 
         String msg = ((num > 10) && (num <100)) ? "Número maior que 10 e menor que 100" : "Número menor ou igual à 10 ou maior que 100";
         System.out.println(msg);
+        //o seguinte também pode ser usado
         //if((num > 10) && (num<100)) {
             //msg = "Número maior que 10 e menor que 100"
         //} else {
@@ -14,6 +15,5 @@ public class Operadores {
         String mesg = ((num % 2) == 0 ) ? "número par" : " número ímpar ";
         System.out.println(mesg);
         
-
     }
 }

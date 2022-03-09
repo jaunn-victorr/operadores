312dea99d487ce3418e7a1663cefb35c560382af
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-03-09 16:53:43 +0000

    save01

---
+++ Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void public static void main(String[] args) {
+        //operadores aritméticos(+= -= *= /= == != > < >= <= ===) 
+        //operadores lógicos (&& -> and/ || -> or/ xor)
+        int num = 50;
+
+        String msg = (num > 10) ? "Número maioq que 10" : "Número menor que 10";
+        System.out.println(msg);
+
+
+
+
+    }
+}

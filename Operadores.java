c1bdb3d22b388f5b3ea36425824e32bc95e682c9
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-03-09 17:41:43 +0000

    save04

--- Operadores.java
+++ Operadores.java
@@ -2,13 +2,18 @@ public class Operadores {
     public static void main(String[] args) {
         //operadores aritméticos(+= -= *= /= == != > < >= <= ===) 
         //operadores lógicos (&& -> and/ || -> or/ xor)
-        int num = 50;
+        int num = 51;
 
-        String msg = (num > 10 && num <100) ? "Número maior que 10 e menor que 100" : "Número menor ou igual à 10 ou maior que 100";
+        String msg = ((num > 10) && (num <100)) ? "Número maior que 10 e menor que 100" : "Número menor ou igual à 10 ou maior que 100";
         System.out.println(msg);
-
-
-
+        //if((num > 10) && (num<100)) {
+            //msg = "Número maior que 10 e menor que 100"
+        //} else {
+           // msg = "Número menor ou igual à 10 ou maior que 100"
+        //}
+        String mesg = ((num % 2) == 0 ) ? "número par" : " número ímpar ";
+        System.out.println(mesg);
+        
 
     }
 }
